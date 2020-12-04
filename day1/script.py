72b584fe9d6c08f684e6714c9755440b4c6bcaa1
schwarzan57163 <schwarzan57163@th-nuernberg.de>
2020-12-05 00:02:40 +0100

    make whole again

--- day1/script.py
+++ day1/script.py
@@ -8,6 +8,8 @@ lines = list(map(lambda x: int(x), lines))
 # force result
 for line1 in lines:
     for line2 in lines:
+        if line1 + line2 == 2020:
+            print('part 1: %s', line1 * line2 * line3)
         for line3 in lines:
             if line1 + line2 + line3 == 2020:
-                print(line1 * line2 * line3)
+                print('part 2: %s', line1 * line2 * line3)
