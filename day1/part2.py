b57aae5756d9ce516c7e233f599039c301c815c3
schwarzan57163 <schwarzan57163@th-nuernberg.de>
2020-12-04 23:49:47 +0100

    split into part 1 and 2

---
+++ day1/part2.py
@@ -0,0 +1,13 @@
+lines = open('input').readlines()
+
+# input conversion
+lines = map(lambda x: x.strip(), lines)
+lines = filter(lambda x: x != '', lines)
+lines = list(map(lambda x: int(x), lines))
+
+# force result
+for line1 in lines:
+    for line2 in lines:
+        for line3 in lines:
+            if line1 + line2 + line3 == 2020:
+                print(line1 * line2 * line3)
