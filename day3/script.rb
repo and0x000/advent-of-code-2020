a1050d568a473813843fdd904ef18fa5b0410773
schwarzan57163 <schwarzan57163@th-nuernberg.de>
2020-12-05 18:24:10 +0100

    rubocop stuff

--- day3/script.rb
+++ day3/script.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal:true
 
-lines = File.open('input').readlines.map( & :strip).reject( & :empty?)
+lines = File.open('input').readlines.map(& :strip).reject(& :empty?)
 
 def move(lines, right = 3, down = 1)
   tree_count = 0
   line_position = 0
-  lines.each_with_index do | line, index | 
-    next unless index % down == 0
+  lines.each_with_index do |line, index|
+    next unless (index % down).zero?
+
     tree_count += 1 if line[line_position].eql?('#')
-    
-    line_position = (line_position + right) % line.size   
+
+    line_position = (line_position + right) % line.size
   end
   tree_count
 end
 
 puts move lines
 
-puts move(lines, 1) * 
-     move(lines, 3) * 
-     move(lines, 5) * 
-     move(lines, 7) * 
+puts move(lines, 1, 1) *
+     move(lines, 3, 1) *
+     move(lines, 5, 1) *
+     move(lines, 7, 1) *
      move(lines, 1, 2)
-
