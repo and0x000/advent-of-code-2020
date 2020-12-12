d4e99f05b091b1e1b19af37a6e329dd033de1e5f
Andreas Schwarz <schwarzan57163@th-nuernberg.de>
2020-12-12 18:04:24 +0100

    add day2 part2 solution

--- day4/script.rb
+++ day4/script.rb
@@ -24,9 +24,37 @@ def validate_part1(passport)
   true
 end
 
+def validate_part2(passport)
+  return false unless passport['byr'].size == 4
+  return false unless (1920..2002).include?(Integer(passport['byr']))
+
+  return false unless passport['iyr'].size == 4
+  return false unless (2010..2020).include?(Integer(passport['iyr']))
+
+  return false unless passport['eyr'].size == 4
+  return false unless (2020..2030).include?(Integer(passport['eyr'].to_i))
+
+  if passport['hgt'].end_with? 'cm'
+    return false unless (150..193).include?(Integer(passport['hgt'].tr('cm', '')))
+  elsif passport['hgt'].end_with? 'in'
+    return false unless (59..76).include?(Integer(passport['hgt'].tr('in', '')))
+  else
+    return false
+  end
+
+  return false unless passport['hcl'].match(/#[0-9,a-f]{6}/)
+
+  return false unless %w[amb blu brn gry grn hzl oth].include? passport['ecl']
+
+  return false unless passport['pid'].size == 9
+
+  true
+end
+
 passport = {}
 
 valid_passports_part1 = []
+valid_passports_part2 = []
 
 lines.each do |line|
   elements = line.split
@@ -36,10 +64,12 @@ lines.each do |line|
     passport[key] = value
   end
 
-  if line.empty?
-    valid_passports_part1.push passport if validate_part1(passport)
-    passport = {} # reset collecting hash
-  end
+  next unless line.empty?
+
+  valid_passports_part1.push passport if validate_part1(passport)
+  valid_passports_part2.push passport if validate_part1(passport) && validate_part2(passport)
+  passport = {} # reset collecting hash
 end
 
 puts valid_passports_part1.count
+puts valid_passports_part2.count
