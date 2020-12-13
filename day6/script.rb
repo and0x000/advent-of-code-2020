968ac7fd0bcc136e482a9970219a37b24ae317fd
Andreas Schwarz <schwarzan57163@th-nuernberg.de>
2020-12-13 13:01:48 +0100

    add day 6 part 2 solution

--- day6/script.rb
+++ day6/script.rb
@@ -2,7 +2,7 @@
 
 groups = File.open('input').read.split(/\n{2}/)
 
-def itterate(groups)
+def itterate_part1(groups)
   return 0 if groups.empty?
 
   group = groups.pop
@@ -10,10 +10,28 @@ def itterate(groups)
 
   group.lines.each do |line|
     line.strip.split('').each do |char|
-       hash[char] = true
+      hash[char] = true
     end
   end
-  hash.size + itterate(groups)
+  hash.size + itterate_part1(groups)
 end
 
-puts itterate groups
+def itterate_part2(groups)
+  return 0 if groups.empty?
+
+  group_lines = groups.pop.lines
+  hash = {}
+
+  group_lines.each do |line|
+    line.strip.split('').each do |char|
+      hash[char] = 0 unless hash.key? char
+      hash[char] += 1
+    end
+  end
+
+  hash.select { |_key, val| val == group_lines.count }.size + itterate_part2(groups)
+end
+
+puts itterate_part1 groups.dup
+
+puts itterate_part2 groups.dup
